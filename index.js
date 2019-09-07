a9ea1f86c4cca34f93bfa7f0531b66df11d3578e
sahusa <sahusa.vinicius@gmail.com>
2019-09-06 23:48:44 -0300

    Testando com now version 1

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(port, err => {
+    if(err){
+        console.log('falha');
+    }else{
+        console.log('tudo bem');
+    }
+});
